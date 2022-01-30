b6713f9e0f91afc67907ca87e714f8cd2f61ac30
Jaroslavs Samcuks <yarcat@gmail.com>
2022-01-30 15:09:57 +0100

    add fsm.go example

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/prog-1/packages
 
 go 1.17
+
+require github.com/yarcat/fsm-go v0.1.2
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"math"
 
 	"github.com/prog-1/packages/areas"
+	"github.com/yarcat/fsm-go"
 )
 
 func main() {
@@ -13,4 +14,17 @@ func main() {
 	fmt.Println("circle   :", areas.Circle(2), 2*2*math.Pi)
 	fmt.Println("square   :", areas.Square(2), 2*2)
 	fmt.Println("rectangle:", areas.Rectangle(2, 3), 2*3)
+
+	const (
+		stInit        fsm.StateType = "stInit"
+		stFinal       fsm.StateType = "stFinal"
+		evInitialized fsm.EventType = "evInitialized"
+	)
+
+	transitions := fsm.Transitions{
+		fsm.When(stInit, evInitialized): stFinal,
+	}
+
+	fsm := fsm.New(stInit, transitions, nil, nil)
+	fsm.Send(evInitialized)
 }
